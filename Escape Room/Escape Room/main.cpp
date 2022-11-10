3bf2235d00a65ae7826b4f1ad9a725137fa120b5
David-Salihu <davidsalihu19@gmail.com>
2022-11-10 18:53:20 +0100

    Removed some files

--- Escape Room/Escape Room/main.cpp
+++ Escape Room/Escape Room/main.cpp
@@ -1,132 +1,48 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-class EscapeRoom : public olc::PixelGameEngine {
-    public:
-        EscapeRoom() {
-            sAppName = "EscapeRoom";
-        }
-        
-        std::string sLevel =
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################"
-            "################";
+class EscapeRoom : public olc::PixelGameEngine
+{
     
-    olc::vf2d vLevelSize = {16, 15};
+public:
+    EscapeRoom() {
+        sAppName = "EscapeRoom";
+    }
+
+    olc::vf2d vLevelSize = {13, 9};
     olc::vf2d vBlockSize = {100, 100};
-    
-    olc::Renderable gfxTiles;
-    
-    struct block
-    {
-        block()
-        {
-            
-        }
-        
-        virtual void DrawSelf(olc::PixelGameEngine* pge, const olc::vi2d& pos, const olc::vi2d& size, const olc::Renderable& skin)
-        {
-            
-        }
-        
-    };
-    
-    struct wall_tiles : public block
-    {
-        void DrawSelf(olc::PixelGameEngine* pge, const olc::vi2d& pos, const olc::vi2d& size, const olc::Renderable& skin) override
-        {
-            pge->DrawPartialSprite(pos * size, skin.Sprite(), olc::vi2d(0, 0) * size, size);
-        }
-    };
-    
-    struct player : public block
-    {
-        void DrawSelf(olc::PixelGameEngine* pge, const olc::vi2d& pos, const olc::vi2d& size, const olc::Renderable& skin) override
-        {
-            pge->DrawPartialSprite(pos * size, skin.Sprite(), olc::vi2d(0, 0) * size, size);
-        }
-    };
-    
-    struct barrel : public block
-    {
-        void DrawSelf(olc::PixelGameEngine* pge, const olc::vi2d& pos, const olc::vi2d& size, const olc::Renderable& skin) override
-        {
-            pge->DrawPartialSprite(pos * size, skin.Sprite(), olc::vi2d(0, 0) * size, size);
-        }
+    olc::vf2d vBarrelSize = {58, 58};
 
-    };
+    olc::Renderable wall;
+    olc::Renderable barrel;
 
+public:
 
-    
-    std::vector<std::unique_ptr<block>> vLevel;
-        
-
-    public:
-    
-        void LoadLevel(int n)
+    void DrawBackground()
+    {
+        for(int y = 0; y < vLevelSize.y; y++)
         {
-            for(int y = 0; y < vLevelSize.y; y++)
+            for(int x = 0; x < vLevelSize.x; x++)
             {
-                for(int x = 0; x < vLevelSize.x; x++)
-                {
-                    switch(sLevel[(y * vLevelSize.x) + x ])
-                    {
-                        case '#':
-                            vLevel.emplace_back(std::make_unique<wall_tiles>());
-                            break;
-                        case 'P':
-                            vLevel.emplace_back(std::make_unique<player>());
-                            break;
-                        case '+':
-                            vLevel.emplace_back(std::make_unique<barrel>());
-                            break;
-                        default:
-                            vLevel.emplace_back(nullptr);
-                    }
-                }
+                DrawSprite(olc::vi2d({x, y}) * vBlockSize, wall.Sprite());
             }
         }
-    
-        bool OnUserCreate() override
-        {
-            gfxTiles.Load("wall.png");
-            LoadLevel(0);
-            return true;
-        }
+    }
 
-        bool OnUserUpdate(float fElapsedTime) override
-        {
-            Clear(olc::BLACK);
-            
-            auto id = [&](olc::vi2d& pos) { return (pos.y * vLevelSize.x) + pos.x; };
-            
-            olc::vi2d vTilePos = {0,0};
-            for (vTilePos.y = 0; vTilePos.y < vLevelSize.y; vTilePos.y++)
-            {
-                for(vTilePos.x = 0; vTilePos.x < vLevelSize.x; vTilePos.x++)
-                {
-                    auto& b = vLevel[id(vTilePos)];
-                    
-                    if(b)
-                    {
-                        b->DrawSelf(this, vTilePos, vBlockSize, gfxTiles);
-                    }
-                }
-            }
-            return true;
-        }
+    bool OnUserCreate() override
+    {
+        wall.Load("wall.png");
+        barrel.Load("1.png");
+        return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+        Clear(olc::BLACK);
+        DrawBackground();
+        //DrawSprite(olc::vi2d({10, 10}) * vBarrelSize, barrel.Sprite());
+        return true;
+    }
 };
 
 
