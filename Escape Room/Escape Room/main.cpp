058e2b32abd9596f52e7c55e18890715b9ad3e46
David-Salihu <davidsalihu19@gmail.com>
2022-11-12 14:57:10 +0100

    Add movement mechanics

--- Escape Room/Escape Room/main.cpp
+++ Escape Room/Escape Room/main.cpp
@@ -13,68 +13,46 @@ public:
     EscapeRoom() {
         sAppName = "EscapeRoom";
     }
-    std::string sLevel =
-        "............."
-        "............."
-        "............."
-        "............."
-        "............."
-        "............."
-        "..........P.."
-        ".............";
-    
-    std::string personPng = "01A.png";
-    std::string personIdlePng = "01.png";
-    
-    olc::vf2d vLevelSize = {13, 9};
-    olc::vf2d vBlockSize = {100, 100};
-    olc::vf2d vPlayerSize = {2, 2};
-    
-    olc::vf2d vPlayerPos = {0, 0};
-    olc::vf2d vPlayerVel = {0, 0};
-    
-    olc::Renderable wall;
-    olc::Renderable wall2;
-
-    olc::Sprite* person = nullptr;
-    olc::Decal* person_dec = nullptr;
-    
-    uint8_t frame = 0;
-    uint8_t frame_count = 0;
-    uint8_t frame_count_idle;
-    
-
+private:
+    std::string sLevel;
+    olc::vi2d vLevelSize;
+    olc::vf2d vCameraPos;
+    olc::vf2d vPlayerPos;
+    olc::vf2d vPlayerVel;
 public:
 
-    void DrawBackground()
-    {
-        for(int y = 0; y < vLevelSize.y; y++)
-        {
-            for(int x = 0; x < vLevelSize.x; x++)
-            {
-                if( y >= 7)
-                    DrawSprite(olc::vi2d({x, y}) * vBlockSize, wall2.Sprite());
-                else
-                    DrawSprite(olc::vi2d({x, y}) * vBlockSize, wall.Sprite());
-            }
-        }
-    }
 
     bool OnUserCreate() override
     {
-        wall.Load("wall.png");
-        wall2.Load("wall2.png");
-            
-            
-        person = new olc::Sprite("01.png");
-        person_dec = new olc::Decal(person);
+        vLevelSize = {64, 16};
+        vCameraPos = {0, 0};
+        vPlayerPos = vCameraPos;
+        
+        sLevel =    "..#######......................................................."
+                    "................................................................"
+                    "................................................................"
+                    "...................................###############.............."
+                    "...................................##..........................."
+                    "................................................................"
+                    "...............................################................."
+                    ".............................####..............................."
+                    "........................#####################..................."
+                    "###########################################...#################."
+                    "..........................................#..#.................."
+                    "............................###############..#.................."
+                    "............................#................#.................."
+                    "............................#.################.................."
+                    "............................###................................."
+                    "................................................................";
+        
         
         return true;
     }
 
     bool OnUserUpdate(float fElapsedTime) override
     {
-        auto GetTile = [&](int x, int y)
+        //Utility Lamdas
+        auto GetTile= [&](int x, int y)
         {
             if(x >= 0 && x < vLevelSize.x && y >= 0 && y < vLevelSize.y)
                 return sLevel[y * vLevelSize.x + x];
@@ -82,117 +60,76 @@ public:
                 return ' ';
         };
         
-        auto SetTile = [&](int x, int y, char c)
+        auto SetTile= [&](int x, int y, char c)
         {
             if(x >= 0 && x < vLevelSize.x && y >= 0 && y < vLevelSize.y)
                 sLevel[y * vLevelSize.x + x] = c;
         };
         
-        vPlayerVel = {0, 0};
+        vPlayerVel = {0,0};
         
-        // Handle input
+        // Handle Input
         if(IsFocused())
         {
             if(GetKey(olc::Key::UP).bHeld)
             {
-                vPlayerVel.y = -6.0f;
+                  vPlayerVel.y = -6.0f;
             }
             if(GetKey(olc::Key::DOWN).bHeld)
             {
-                vPlayerVel.y = 6.0f;
+                  vPlayerVel.y = 6.0f;
             }
             if(GetKey(olc::Key::LEFT).bHeld)
             {
-                if ( frame > 0 && frame < 10)
-                {
-                    personPng[0] = char(int(frame/10) + 48);
-                    personPng[1] = char(frame + 48);
-                }
-                else{
-                    personPng[0] = char(int(frame/10) + 48);
-                    personPng[1] = char(int(frame-10) + 48);
-                }
-                person = new olc::Sprite(personPng);
-                person_dec = new olc::Decal(person);
-                vPlayerVel.x = -2.0f;
-                if(!(frame_count % 4))
-                    frame = (++frame) % 14;
-                if(frame == 0)
-                    frame = 1;
-                frame_count++;
+                  vPlayerVel.x = -6.0f;
             }
             if(GetKey(olc::Key::RIGHT).bHeld)
             {
-                if ( frame > 0 && frame < 10)
-                {
-                    personPng[0] = char(int(frame/10) + 48);
-                    personPng[1] = char(frame + 48);
-                }
-                else{
-                    personPng[0] = char(int(frame/10) + 48);
-                    personPng[1] = char(int(frame-10) + 48);
-                }
-                person = new olc::Sprite(personPng);
-                person_dec = new olc::Decal(person);
-                vPlayerVel.x = 2.0f;
-                if(!(frame_count % 4))
-                    frame = (++frame) % 14;
-                if(frame == 0)
-                    frame = 1;
-                frame_count++;
+                  vPlayerVel.x = 6.0f;
             }
-            
         }
         
-        if(!(frame_count_idle % 5) && !(GetKey(olc::Key::RIGHT).bHeld) && !(GetKey(olc::Key::LEFT).bHeld) )
-        {
-            if ( frame > 0 && frame < 10)
-            {
-                personIdlePng[0] = char(int(frame/10) + 48);
-                personIdlePng[1] = char(frame + 48);
-            }
-            else if(frame >= 10 && frame < 20){
-                personIdlePng[0] = char(int(frame/10) + 48);
-                personIdlePng[1] = char(int(frame-10) + 48);
-            }
-            else{
-                personIdlePng[0] = char(int(frame/10) + 48);
-                personIdlePng[1] = char(int(frame-20) + 48);
-            }
-            
-            person = new olc::Sprite(personIdlePng);
-            person_dec = new olc::Decal(person);
-            
-            frame = (++frame) % 26;
-            if(frame == 0)
-                frame = 1;
-            //std::cout<<personPng<<std::endl;
-        }
-        
-        frame_count_idle++;
         vPlayerPos.x = vPlayerPos.x + vPlayerVel.x * fElapsedTime;
         vPlayerPos.y = vPlayerPos.y + vPlayerVel.y * fElapsedTime;
         
-        Clear(olc::BLACK);
-        DrawBackground();
-
+        vCameraPos.x = vPlayerPos.x;
+        vCameraPos.y = vPlayerPos.y;
         
-        //Draw Tiles
-        for(int x = 0; x < vLevelSize.x; x++)
+        // Draw Level
+        olc::vi2d vTileSize = {16, 16};
+        olc::vi2d vVisibleTiles = { ScreenWidth()/vTileSize.x, ScreenHeight()/vTileSize.y };
+        
+        // Calculate Top Leftmost visible tile ( camera represents the middle of the screen)
+        olc::vf2d vOffset = { vCameraPos.x - ( (float)vVisibleTiles.x/2.0f), vCameraPos.y - ( (float)vVisibleTiles.y/2.0f) };
+        
+        // Clamp camera to game boundaries
+        if (vOffset.x < 0) vOffset.x = 0;
+        if (vOffset.y < 0) vOffset.y = 0;
+        if (vOffset.x > vLevelSize.x - vVisibleTiles.x) vOffset.x = vLevelSize.x - vVisibleTiles.x;
+        if (vOffset.y > vLevelSize.y - vVisibleTiles.y) vOffset.y = vLevelSize.y - vVisibleTiles.y;
+        
+        // Draw visible tile map
+        for(int x = 0; x < vVisibleTiles.x; x++)
         {
-            for(int  y = 0; y < vLevelSize.y; y++)
+            for(int y = 0; y < vVisibleTiles.y; y++)
             {
-                char sTileType = GetTile(x, y);
-                switch(sTileType)
+                char sTileID = GetTile(x + vOffset.x, y + vOffset.y);
+                switch(sTileID)
                 {
-                    case 'P':
-                        DrawDecal(olc::vf2d{vPlayerPos.x, vPlayerPos.y} * vBlockSize, person_dec,vPlayerSize);
+                    case '.':
+                        FillRect(x * vTileSize.x, y * vTileSize.y, (x + 1) * vTileSize.x, (y + 1) * vTileSize.y, olc::CYAN);
+                        break;
+                    case '#':
+                        FillRect(x * vTileSize.x, y * vTileSize.y, (x + 1) * vTileSize.x, (y + 1) * vTileSize.y, olc::RED);
                         break;
                     default:
                         break;
                 }
             }
         }
+        
+        //Draw Player
+        FillRect((vPlayerPos.x - vOffset.x) * vTileSize.x, (vPlayerPos.y - vOffset.y) * vTileSize.y, vTileSize.x, vTileSize.y, olc::BLUE);
         
         return true;
     }
@@ -201,7 +138,7 @@ public:
 
 int main(int argc, char const *argv[]) {
     EscapeRoom demo;
-    if (demo.Construct(1280, 848, 1, 1))
+    if (demo.Construct(160, 120, 8, 8))
         demo.Start();
 
     return 0;
